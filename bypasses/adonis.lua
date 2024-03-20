0254f527c8315b3a3548c67cf768d7195d00745a
andrvew <160489710+andrvew@users.noreply.github.com>
2024-03-20 17:36:45 -0400

    Create adonis.lua

---
+++ bypasses/adonis.lua
@@ -0,0 +1,9 @@
+    --[[ Credits to whoever the Adonis Bypass <3 ]]
+    for k, v in pairs(getgc(true)) do
+        if pcall(function() return rawget(v, 'indexInstance') end) then
+            if type(rawget(v, 'indexInstance')) == 'table' and (rawget(v, 'indexInstance'))[1] == 'kick' then
+                task.wait()
+                v.tvk = {'kick', function() return workspace:WaitForChild('') end}
+            end
+        end
+    end
